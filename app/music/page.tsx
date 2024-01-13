3ab00b272d82e687d82625cef96755c996a75b40
mohd jami <your_email@example.com>
2024-01-14 00:12:05 +0530

    Added Dashboard Music and Admin

--- app/music/page.tsx
+++ app/music/page.tsx
@@ -1,7 +1,15 @@
 import Music from "@/components/Music";
+
+import { authOptions } from "@/lib/auth";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 import React from "react";
 
-const MusicPage = () => {
+const MusicPage = async () => {
+  const session = await getServerSession(authOptions);
+  if (!session) {
+    redirect("/");
+  }
   return <Music />;
 };
 
